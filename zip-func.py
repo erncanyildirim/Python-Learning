ec68633a5def7b2a1b6b9ade885b1ba582fbf20e
erncanyildirim <erncanyildirim@gmail.com>
2024-02-12 23:07:56 +0300

    Create zip-func.py

---
+++ zip-func.py
@@ -0,0 +1,8 @@
+# zip fonksiyonu
+
+liste1 = list("Python")
+liste2 = [1, 2, 3, 4]
+liste3 = ["a", "b", "c", "d"]
+
+zip_sonuc = list(zip(liste1, liste2, liste3))    #list, tuple veya set olarak yazdırabiliriz
+print(zip_sonuc)
